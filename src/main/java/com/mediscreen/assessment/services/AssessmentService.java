18be2e532f9061afcf7b10315f8bd6ef3cc59049
Aimé Malcorps <aime.malcorps@neuf.fr>
2023-06-19 21:39:10 +0200

    Refactoring for clean code

--- src/main/java/com/mediscreen/assessment/services/AssessmentService.java
+++ src/main/java/com/mediscreen/assessment/services/AssessmentService.java
@@ -8,12 +8,12 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.mediscreen.assessment.dto.AssessmentDTO;
 import com.mediscreen.assessment.dto.NoteDTO;
 import com.mediscreen.assessment.dto.PatientDTO;
+import com.mediscreen.assessment.proxies.DemographicProxy;
 import com.mediscreen.assessment.proxies.HistoryNotesProxy;
 
 @Service
@@ -21,42 +21,70 @@ public class AssessmentService {
 
 	@Autowired
 	HistoryNotesProxy historyNotesProxy;
+	
+	@Autowired
+	DemographicProxy demogrphicProxy;
 
 	private Logger logger = LoggerFactory.getLogger(AssessmentService.class);
 
 	public static final List<String> keyWords = Arrays.asList("Hémoglobine A1C", "Microalbumine", "Taille", "Poids", "Fumeur",
 			"Anormal", "Cholestérol", "Vertige", "Rechute", "Réaction", "Anticorps");
 
-	public String generateAssessment(PatientDTO patientDTO) {
+	
+	public AssessmentDTO getPatientByFamilly(String familly) {
+		PatientDTO patientDTO = new PatientDTO();
 		
-		String risque = "None";
 		try {
-			List<NoteDTO> patientNotes = historyNotesProxy.getNotesByPatientId(patientDTO.getId());
-			
-			if (patientNotes != null) {
-				int keyWordOccurency = calculateKeyWordPresence(patientNotes);
-				logger.info("keyWordOccurency = " + keyWordOccurency);
-				// Aucun risque de diabete
-				if (keyWordOccurency == 0) {
-					return risque;
-					
-				} else {
-					// si le patient a plus de 30 ans
-					if (Period.between(LocalDate.parse(patientDTO.getDob()), LocalDate.now()).getYears() > 30) {
-						logger.info("PLUS de 30 ans");
-						risque = assessmentMoreThan30(keyWordOccurency);
-					// moins de 30 ans
-					} else {
-						logger.info("MOINS de 30 ans");
-						risque = assessmentLessThan30(keyWordOccurency, patientDTO.getSex());
-					}
-				}
-			}
-			return risque;
-		} catch(Exception e) {
+			patientDTO = demogrphicProxy.getPatientByName(familly);
+			return generateAssessment(patientDTO);
+		} catch (Exception e) {
 			return null;
 		}
+	}
+
+	public AssessmentDTO generateAssessment(PatientDTO patientDTO) {
 		
+		AssessmentDTO assessmentDTO = new AssessmentDTO();
+		
+		String risque = "None";
+		
+		if (patientDTO != null) {
+			
+			try { 
+				
+				assessmentDTO.setPatientInfo(generatePatientInfo(patientDTO));
+				
+				List<NoteDTO> patientNotes = historyNotesProxy.getNotesByPatientId(patientDTO.getId());
+				
+				if (patientNotes != null) {
+					int keyWordOccurency = calculateKeyWordPresence(patientNotes);
+					logger.info("keyWordOccurency = " + keyWordOccurency);
+					// Aucun risque de diabete
+					if (keyWordOccurency == 0) {
+						assessmentDTO.setResult(risque);
+						return assessmentDTO;
+					} else {
+						// si le patient a plus de 30 ans
+						if (Period.between(LocalDate.parse(patientDTO.getDob()), LocalDate.now()).getYears() > 30) {
+							logger.info("PLUS de 30 ans");
+							risque = assessmentMoreThan30(keyWordOccurency);
+						// moins de 30 ans
+						} else {
+							logger.info("MOINS de 30 ans");
+							risque = assessmentLessThan30(keyWordOccurency, patientDTO.getSex());
+						}
+					}
+				}
+				
+				assessmentDTO.setResult(risque);
+				return assessmentDTO;
+				
+			} catch(Exception e) {
+				return null;
+			}
+		} else {
+			return null;
+		}
 	}
 	
 	// Calcul le risque pour un patient de moins de 30 ans
@@ -115,6 +143,17 @@ public class AssessmentService {
 			}
 		}
 		return keyWordOccurency;
+	}
+	
+	public String generatePatientInfo(PatientDTO patientDTO) {
+		
+		try {
+			int age = Period.between(LocalDate.parse(patientDTO.getDob()), LocalDate.now()).getYears();
+			return patientDTO.getFamilly() + " " + patientDTO.getGiven() +
+					" (age " + age + ")";
+		} catch (Exception e) {
+			return patientDTO.getFamilly() + " " + patientDTO.getGiven();
+		}
 	}
 
 }
